670a535d893ccbff10fe8a94a8ce750c9577e9f9
loubana alarabi <loubana@dconfig.com>
2021-11-28 14:56:26 +0300

    fix style

--- src/components/EndGame/EndGame.jsx
+++ src/components/EndGame/EndGame.jsx
@@ -6,8 +6,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const EndGame = () => {
   return (
     <div className="endGamePage">
-      <h1 class="endGameText fire"> Next Level </h1>
-      <Button href="/home"  variant="danger" size="lg">
+      <h1 class="endGameText fire"><a href="/chapters"> Next Level </a></h1>
+      <style type="text/css">
+      {`
+        .btn-flat {
+          background-color: #6b202a;
+          color: white;
+        }
+
+        .btn-xxl {
+          padding: 1rem 1.5rem;
+          font-size: 2rem;
+        }
+      `}
+      </style>
+      <Button href="/home" variant="flat" size="xxl">
           Return To Home Page
       </Button>
     </div>
