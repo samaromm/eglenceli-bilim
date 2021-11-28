233f2c11d7ef8000829008cf7c8e603e8d75e292
rashaabdulrazzak <rasha.abdulrazzak.com>
2021-11-28 15:02:50 +0300

    add chapter section

---
+++ src/assets/data/planets.js
@@ -0,0 +1,51 @@
+ import planet1 from '../../assets/img/planet-1.png';
+ import planet2 from '../../assets/img/planet-2.png';
+ import planet3 from '../../assets/img/planet-3.png';
+ import planet4 from '../../assets/img/planet-4.png';
+ import planet5 from '../../assets/img/planet-5.png';
+ import planet6 from '../../assets/img/planet-6.png';
+ import planet7 from '../../assets/img/planet-7.png';
+ import planet8 from '../../assets/img/planet-8.png';
+
+ const PLANETS = [{
+     id: 1,
+     name: 'KABARIK ŞAMATA',
+     photo: planet1
+   },
+   {
+     id: 2,
+     name: 'ÇILGIN DEDEKTİFLER',
+     photo: planet2
+   },
+   {
+     id: 3,
+     name: 'ÇILGIN DEDEKTİFLER',
+     photo: planet3
+   },
+   {
+     id: 4,
+     name: 'ÇILGIN DEDEKTİFLER',
+     photo: planet4
+   },
+   {
+     id: 5,
+     name: 'ÇILGIN DEDEKTİFLER',
+     photo: planet5
+   },
+   {
+     id: 6,
+     name: 'ÇILGIN DEDEKTİFLER',
+     photo: planet6
+   },
+   {
+     id: 7,
+     name: 'ÇILGIN DEDEKTİFLER',
+     photo: planet7
+   },
+   {
+     id: 8,
+     name: 'ÇILGIN DEDEKTİFLER',
+     photo: planet8
+   }
+ ];
+ export default PLANETS
